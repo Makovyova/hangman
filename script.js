4d9d11fa35fd6b6559bb8a58d7b99a2934a93f98
Kate Makovieva <94247189+Makovyova@users.noreply.github.com>
2024-04-18 00:23:38 +0300

    docs: create file sript.js which contain all body tags

---
+++ script.js
@@ -0,0 +1,12 @@
+//define body tag
+const body=document.querySelector('body')
+
+//create elements for the gallows part
+const container=document.createElement('div')
+div.className="container"
+body.append(container)
+
+const gallow=document.createElement('img')
+img.src="gallows.svg"
+img.src="Gallow"
+container.append(img)
